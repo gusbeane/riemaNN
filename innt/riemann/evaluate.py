4cb88e25aef53e838f6d1b14b597edd16bd4f87f
Gus Beane <angus.beane@nyu.edu>
2026-06-03 15:50:29 -0400

    Make rel error floor

--- innt/riemann/evaluate.py
+++ innt/riemann/evaluate.py
@@ -38,7 +38,7 @@ def errors_on(
     """
     F_pred = jax.vmap(lambda r: F_pred_fn(F_net, r))(x_eval)
     F_exact = jax.vmap(F_true_fn)(x_eval)
-    rel = jnp.abs(F_pred - F_exact) / jnp.abs(F_exact)
+    rel = jnp.abs(F_pred - F_exact) / (jnp.abs(F_exact) + rel_tol)
 
     median_rel_channels = jnp.median(rel, axis=0)
     max_rel_channels = jnp.max(rel, axis=0)
